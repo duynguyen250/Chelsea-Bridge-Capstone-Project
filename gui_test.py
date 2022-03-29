5621d25b7e900af23d76002147c493890e743a1a
Duy Nguyen <nguyenanhduy2503@gmail.com>
2022-03-28 23:54:23 -0400

    added readonly entry, docstrings, cosmetic changes

--- gui_test.py
+++ gui_test.py
@@ -4,29 +4,54 @@ import pandas as pd
 import numpy as np
 from joblib import load
 from sklearn.linear_model import LinearRegression 
-
-lr_model = load(r"C:\Users\Duy Nguyen\Desktop\Chelsea Bridge\Chelsea-Bridge-Capstone-Project\lr_model.joblib")
-
+from tkinter import StringVar, messagebox
+from tkinter import filedialog
 
 root = tk.Tk()
 
+frame_title = tk.Frame(root)
+frame_title.grid(row=0,column=0)
+
 frame_date = tk.LabelFrame(root,text='Date',font=('Helvatica',10,'bold'),relief=tk.SUNKEN)
-frame_date.grid(row=0,column=0)
+frame_date.grid(row=1,column=0)
 
 frame_weather = tk.LabelFrame(root,text='Weather',font=('Helvatica',10,'bold'),relief=tk.SUNKEN)
-frame_weather.grid(row=1,column=0)
+frame_weather.grid(row=2,column=0)
 
 frame_vessel = tk.LabelFrame(root,text='Vessels and Time',font=('Helvatica',10,'bold'),relief=tk.SUNKEN)
-frame_vessel.grid(row=2,column=0)
+frame_vessel.grid(row=3,column=0)
+
+def browse_model():
+    """Ask user for joblib model directory and set path to global variable
+    """
+    global model_path
+    model_path = tk.filedialog.askopenfilename()
+   
 
 def button_click():
-    '''Void function for predicting datetime using linear regression model'''
+    
+    """Void function for generating linear regression result based on the inputted values
+    """
     date = entry_date.get()
     temp = entry_temp.get()
     tide = entry_tide.get()
     tug = entry_tug.get()
     barge = entry_barge.get()
     tanker = entry_tanker.get()
+    try:
+        date = pd.to_datetime(date)
+    except:
+        messagebox.showerror(title='Python Error',message='Please ensure the Today\'s date is in yyyy/mm/dd format')
+    for var in [tug,barge,tanker]:
+        try:     
+            var = int(var)
+        except:
+            messagebox.showerror(title='Python Error',message='Please ensure the value for Tug, Barge and Tanker are integers')
+    for var in [temp,tide]:
+        try:     
+            var = float(var)
+        except:
+            messagebox.showerror(title='Python Error',message='Please ensure the value for Temperature and Predicted Tide are decimal numbers')
     
     #eta_bridge = date_convert(entry_eta_bridge.get(),'hour')
     day = date_convert(date,'day')
@@ -35,10 +60,31 @@ def button_click():
     
     row = [day, month, year, temp, tide, tug, barge, tanker, 1, 1, sunrise.get(), sunset.get(), clear.get(), cloudy.get(), overcast.get(), snow.get()]
     minute,hour = predict_date(row)
-    tk.Label(root,text=f'The predicted time is {hour}:{minute:.0f}').grid(row=21,column=0)
+    #tk.Label(root,text=f'The lift predicted time is {hour}:{minute:.0f}',font=('Helvatica',10,'bold'),state='readonly').grid(row=21,column=0)
+    predict_text = StringVar()
+    predict_text.set(f'{hour}:{minute:.0f}')
+    tk.Entry(root,font=('Helvatica',10,'bold'),state='readonly',textvariable=predict_text,width=5,foreground='red').grid(row=22,column=0)
+    tk.Label(root, text='The Predicted Time of the Bridge Lift is:',font=('Helvatica',10)).grid(row=21,column=0)
+    
 
 def predict_date(row):
-    """Return the predict value"""
+    """Takes in list of features and convert it into a pandas DataFrame
+
+    Args:
+        row: list 
+            List contains all the features for prediction.
+
+
+    Returns: 
+        Pandas DataFrame
+            List of all features with specific column names
+    """
+    
+    try:
+        lr_model = load(model_path)
+    except:
+        messagebox.showerror(title='Python Error',message='Please select the correct model file')
+        
     df = pd.DataFrame(row).T
     df.columns = ['Day', 'Month', 'Year', 'temp', 'Predicted (ft)', 'Tug', 'Barge',
        'Tanker', 'Previous Lift', 'instances_1hr', 'sunrise_flag',
@@ -108,38 +154,42 @@ entry_tanker.grid(row=5,column=1)
 
 sunrise = tk.IntVar()
 check_sunrise = tk.Checkbutton(frame_weather,text='Sunrise',variable=sunrise)
-check_sunrise.grid(row=7,column=0)
+check_sunrise.grid(row=7,column=0,sticky=tk.W)
 
 sunset = tk.IntVar()
 check_sunset = tk.Checkbutton(frame_weather,text='Sunset',variable=sunset)
-check_sunset.grid(row=8,column=0)
+check_sunset.grid(row=8,column=0,sticky=tk.W)
 
 clear = tk.IntVar()
 check_clear = tk.Checkbutton(frame_weather,text='Clear',variable=clear)
-check_clear.grid(row=9,column=0)
+check_clear.grid(row=9,column=0,sticky=tk.W)
 
 cloudy = tk.IntVar()
 check_cloudy = tk.Checkbutton(frame_weather,text='Cloudy',variable=cloudy)
-check_cloudy.grid(row=10,column=0)
+check_cloudy.grid(row=10,column=0,sticky=tk.W)
 
 overcast = tk.IntVar()
 check_overcast = tk.Checkbutton(frame_weather,text='Overcast',variable=overcast)
-check_overcast.grid(row=11,column=0)
+check_overcast.grid(row=11,column=0,sticky=tk.W)
 
 snow = tk.IntVar()
 check_snow = tk.Checkbutton(frame_weather,text='Snow',variable=snow)
-check_snow.grid(row=12,column=0)
+check_snow.grid(row=12,column=0,sticky=tk.W)
 
 
 tk.Button(root, text='Predict',command=button_click,padx=30,font=('Helvatica',10,'bold'),relief=tk.RAISED).grid(row=20,column=0)
+tk.Button(frame_title, text='Select Model File',command=browse_model,padx=30,font=('Helvatica',10,'bold'),relief=tk.RAISED).grid(row=20,column=0)
 tk.Label(frame_date, text="Today's date:").grid(row=0,column=0)
-tk.Label(frame_weather, text="Temperature:").grid(row=1,column=0)
-tk.Label(frame_weather, text="Tide:").grid(row=2,column=0)
+tk.Label(frame_weather, text="Temperature:").grid(row=1,column=0,sticky=tk.W)
+tk.Label(frame_weather, text="Predicted Tide:").grid(row=2,column=0)
 tk.Label(frame_vessel, text="Tug:").grid(row=3,column=0)
 tk.Label(frame_vessel, text="Barge:").grid(row=4,column=0)
 tk.Label(frame_vessel, text="Tanker:").grid(row=5,column=0)
+tk.Label(frame_title, text='Chelsea Bridge Lift \n Bridge Time Predictor',font=('Helvatica',13,'bold')).grid(row=0,column=0)
 #tk.Label(frame_vessel, text="ETA Bridge:").grid(row=6,column=0)
 
+
 entry_date.focus()
-root.title('Linear Regression Model')
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 root.mainloop()
